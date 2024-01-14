0924d16cee9ffc3f2bdb92c73249f455bab733d2
Ahmad Hassan <mi010203269@gmail.com>
2024-01-15 01:17:54 +0200

    init Producer Consumer DP

--- backend/src/main/java/com/concurrencysimulation/backend/Models/Machine.java
+++ backend/src/main/java/com/concurrencysimulation/backend/Models/Machine.java
@@ -1,41 +1,70 @@
 package com.concurrencysimulation.backend.Models;
 
 import java.awt.Color;
+import java.util.ArrayList;
 
-public class Machine {
-    private int id;
-    private int time;
-    private Color color;
+public class Machine extends Thread{
+    private int machineId;
+    private Product currentProduct;   // Product that is currently being processed
+    private Queue queue;
+    private ArrayList<Queue> queues = new ArrayList<Queue>();
 
-    public Machine(int id, int time) {
-        this.id = id;
-        this.time = time;
-        Color defaultColor = new Color(94, 190, 247);
-        this.color = defaultColor;
+    public Machine(int id, Queue queue, ArrayList<Queue> queues) {
+        this.machineId = id;
+        this.queue = queue;
+        this.queues = queues;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public int getTime() {
-        return time;
+    public int getMachineId() {
+        return machineId;
     }
 
     public Color getColor() {
-        return color;
+        return currentProduct.getColor();
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setCurrentProduct(Product product) {
+        this.currentProduct = product;
     }
 
-    public void setTime(int time) {
-        this.time = time;
+    public void finished(){
+        this.currentProduct = null;
+        for(Queue queue : queues){
+            synchronized(queue){
+                if(queue.getProducts().size()>0){
+                    Product product = queue.getProducts().get(0);
+                    queue.removeProduct(product);
+                    this.serve(product);
+                    return;
+                }
+            }
+        }
+        // TODO: notify queues that machine is free
+        for (Queue queue : queues) {
+            // queue.notifyMachineFree(this);
+        }
     }
 
-    public void setColor(Color color) {
-        this.color = color;
+    public void run() {
+        // create random processing time
+        int processingTime = (int) (Math.random() * 5000);
+        try {
+            Thread.sleep(processingTime);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        synchronized (queue) {
+            // add product to queue
+            queue.addProduct(currentProduct);
+        }
+        this.finished();
+
+    }
+
+    public void serve(Product product) {
+        this.setCurrentProduct(product);
+        this.start();
     }
 
 }
