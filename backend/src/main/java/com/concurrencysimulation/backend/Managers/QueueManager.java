bf80b740ca72bbe1d93854b875962ad8c8c70d20
Ahmad Hassan <43483116+AhmadHassan1001@users.noreply.github.com>
2024-01-15 23:25:53 +0200

    Pros cons (#2)
    * Adding Models
    
    * init Producer Consumer DP
    
    * tested concurrency
    
    ---------
    
    Co-authored-by: AbdElRahman Osama <bodyusama207@gmail.com>
    Co-authored-by: AbdElRahman Osama <52374998+AbdElRahmanOsama182@users.noreply.github.com>

--- backend/src/main/java/com/concurrencysimulation/backend/Managers/QueueManager.java
+++ backend/src/main/java/com/concurrencysimulation/backend/Managers/QueueManager.java
@@ -23,7 +23,8 @@ public class QueueManager {
     public Queue createQueue() {
         Queue queue = new Queue(nextId);
         nextId++;
-        queues.put(queue.getId(), queue);
+        queues.put(queue.getQueueId(), queue);
+
         return queue;
     }
 
--- backend/src/main/java/com/concurrencysimulation/backend/Models/Machine.java
+++ backend/src/main/java/com/concurrencysimulation/backend/Models/Machine.java
@@ -1,41 +1,108 @@
 package com.concurrencysimulation.backend.Models;
 
 import java.awt.Color;
+import java.util.ArrayList;
 
-public class Machine {
-    private int id;
-    private int time;
-    private Color color;
+import com.concurrencysimulation.backend.Models.Queue;
+import com.concurrencysimulation.backend.Models.Product;
 
-    public Machine(int id, int time) {
-        this.id = id;
-        this.time = time;
-        Color defaultColor = new Color(94, 190, 247);
-        this.color = defaultColor;
+public class Machine extends Thread{
+    private int machineId;
+    private Product currentProduct;   // Product that is currently being processed
+    private Queue queue;
+    private ArrayList<Queue> queues = new ArrayList<Queue>();
+
+    public Machine(int id) {
+        this.machineId = id;
+        this.queue = queue;
+        this.queues = queues;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public int getTime() {
-        return time;
+    public int getMachineId() {
+        return machineId;
     }
 
     public Color getColor() {
-        return color;
+        return currentProduct.getColor();
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setCurrentProduct(Product product) {
+        this.currentProduct = product;
     }
 
-    public void setTime(int time) {
-        this.time = time;
+    public void finished(){
+        this.currentProduct = null;
+        for(Queue queue : queues){
+            synchronized(queue){
+                if(queue.getProducts().size()>0){
+                    Product product = queue.getProducts().get(0);
+                    queue.removeProduct(product);
+                    System.out.println("Product " + product.getId() + " is being served in machine " + this.machineId);
+                    
+                    this.setCurrentProduct(product);
+                    this.run();
+                    return;
+                }
+            }
+        }
+        // TODO: notify queues that machine is free
+        for (Queue queue : queues) {
+            // queue.notifyMachineFree(this);
+        }
     }
 
-    public void setColor(Color color) {
-        this.color = color;
+    public void run() {
+
+        // create random processing time
+        int processingTime = (int) (Math.random() * 5000);
+        System.out.println("Processing time: " + processingTime + " in machine " + this.machineId);
+        try {
+            Thread.sleep(processingTime);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Product " + currentProduct.getId() + " is finished"+ " in machine " + this.machineId);
+
+        synchronized (queue) {
+            // add product to queue
+            queue.addProduct(currentProduct);
+        }
+        this.finished();
+
+    }
+
+    public void serve(Product product) {
+        this.setCurrentProduct(product);
+        this.start();
+    }
+
+    public void setTargetQueue(Queue queue){
+        this.queue = queue;
+    }
+
+    public void addQueue(Queue queue){
+        this.queues.add(queue);
+    }
+
+
+    public static void main(String[] args) {
+        Queue queue = new Queue(1);
+        Queue queue2 = new Queue(2);
+        
+        Machine machine = new Machine(1);
+        machine.addQueue(queue);
+        machine.setTargetQueue(queue2);
+        Product product = new Product(Color.RED,1);
+        machine.serve(product);
+
+        Machine machine2 = new Machine(2);
+        machine2.addQueue(queue2);
+        machine2.setTargetQueue(queue);
+        Product product2 = new Product(Color.BLUE,2);
+        machine2.serve(product2);
+
+
     }
 
 }
