a65b8e3b9e2781594435376f9f707aeb420cbfcc
Ahmad Hassan <mi010203269@gmail.com>
2024-01-16 21:54:40 +0200

    add concurrency to queue

--- backend/src/main/java/com/concurrencysimulation/backend/Models/Queue.java
+++ backend/src/main/java/com/concurrencysimulation/backend/Models/Queue.java
@@ -3,6 +3,8 @@ package com.concurrencysimulation.backend.Models;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
+import javax.crypto.Mac;
+
 public class Queue extends Thread {
     private int queueId;
     private ArrayList<Machine> machines = new ArrayList<Machine>();
@@ -23,6 +25,19 @@ public class Queue extends Thread {
     public ArrayList<Product> getProducts() {
         return products;
     }
+
+    public Product getTopProduct() {
+        synchronized (this) {
+            if (products.size() > 0) {
+                System.out.println("Product size: " + products.size() + " of queue " + this.getQueueId());
+                Product lastProduct = products.get(products.size() - 1);
+                products.remove(products.size() - 1);
+                return lastProduct;
+            }
+            return null;
+        }
+    }
+
     public void setMachines(ArrayList<Machine> machines) {
         this.machines = machines;
     }
@@ -40,11 +55,15 @@ public class Queue extends Thread {
     }
 
     public void addProduct(Product product) {
-        products.add(product);
+        // get queue thread state
+        System.out.println("Thread state: " + this.getState()+" of queue "+this.getQueueId());
+        synchronized (this) {
+            products.add(product);
+        }
     }
 
     public void removeProduct(Product product) {
         products.remove(product);
     }
-
+    
 }
